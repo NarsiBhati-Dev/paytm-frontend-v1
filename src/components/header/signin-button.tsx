55c1d9250453b0a3219544c5f48190ff77196b9c
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-02-13 22:54:17 +0530

    i changes

--- src/components/header/signin-button.tsx
+++ src/components/header/signin-button.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const SigninButton = () => {
   return (
-    <button className="flex justify-center items-center gap-1 bg-[#002970] rounded-full p-[2px] md:pr-[15px] text-white text-sm">
+    <Link
+      href={"/signin"}
+      className="flex justify-center items-center gap-1 bg-[#002970] rounded-full p-[2px] md:pr-[15px] text-white text-sm"
+    >
       <Image src={"/logoutImg.svg"} alt={"signin"} width={32} height={32} />
       <span className="md:block hidden">Sign in</span>
-    </button>
+    </Link>
   );
 };
 
